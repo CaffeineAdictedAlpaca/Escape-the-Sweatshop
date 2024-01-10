57de82e13e49e5de6f32deebdceaa4f38fa15ab7
Adrian <113990519+AdrianDeveloper@users.noreply.github.com>
2024-01-10 15:09:24 +0100

    MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button StartButton;
+    public Button SettingsButton;
+    public Button QuitButton;
+    private void Start()
     {
-        
+        Button start = StartButton.GetComponent<Button>();
+        start.onClick.AddListener(StartGame);
+        Button settings = SettingsButton.GetComponent<Button>();
+        settings.onClick.AddListener(Settings);
+        Button quit = QuitButton.GetComponent<Button>();
+        quit.onClick.AddListener(QuitGame);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void StartGame()
     {
-        
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+    private void Settings()
+    {
+
+    }
+    private void QuitGame()
+    {
+        Application.Quit();
     }
 }
