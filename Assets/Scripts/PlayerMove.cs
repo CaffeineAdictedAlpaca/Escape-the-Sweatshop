216278aba43f2bf83e32b2cd41406fd51724e8e1
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2024-01-10 15:15:32 +0100

    coment

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,7 +10,6 @@ public class PlayerMove : MonoBehaviour
     float fullSpeed;
     [SerializeField]
     Rigidbody2D player;
-
     public float rotationSpeed = 5f;
     // Start is called before the first frame update
     void Start()
@@ -22,10 +21,10 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))//if the W key is pressed add a force upp 
+        if (Input.GetKey(KeyCode.W))//if the W key is pressed add a force upp -Sixten
         {
             player.AddForce(Vector2.up * speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player -Sixten
             {
                 speed = halfSpeed;
             }
@@ -34,10 +33,10 @@ public class PlayerMove : MonoBehaviour
                 speed = fullSpeed;
             }
         }
-        if (Input.GetKey(KeyCode.S))//if the S key is pressed add a force down 
+        if (Input.GetKey(KeyCode.S))//if the S key is pressed add a force down -Sixten
         {
             player.AddForce(Vector2.down * speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player -Sixten
             {
                 speed = halfSpeed;
             }
@@ -46,10 +45,10 @@ public class PlayerMove : MonoBehaviour
                 speed = fullSpeed;
             }
         }
-        if (Input.GetKey(KeyCode.D))//if the D key is pressed add a force right 
+        if (Input.GetKey(KeyCode.D))//if the D key is pressed add a force right -Sixten
         {
             player.AddForce(Vector2.right * speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))//if moving in more than one direction reduce the speed of the player
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))//if moving in more than one direction reduce the speed of the player -Sixten
             {
                 speed = halfSpeed;
             }
@@ -58,10 +57,10 @@ public class PlayerMove : MonoBehaviour
                 speed = fullSpeed;
             }
         }
-        if (Input.GetKey(KeyCode.A))//if the A key is pressed add a force left 
+        if (Input.GetKey(KeyCode.A))//if the A key is pressed add a force left -Sixten
         {
             player.AddForce(Vector2.left * speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))//if moving in more than one direction reduce the speed of the player -Sixten
             {
                 speed = halfSpeed;
             }
@@ -71,16 +70,16 @@ public class PlayerMove : MonoBehaviour
             }
         }
 
-        // Get the direction of movement
+        // Get the direction of movement -Sixten
         Vector2 direction = GetComponent<Rigidbody2D>().velocity;
 
-        // Calculate the angle in radians
+        // Calculate the angle in radians -Sixten
         float angle = Mathf.Atan2(-direction.x, direction.y) * Mathf.Rad2Deg;
 
-        // Create a rotation quaternion based on the angle
+        // Create a rotation quaternion based on the angle -Sixten
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        // Rotate towards the calculated angle smoothly
+        // Rotate towards the calculated angle smoothly -Sixten
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
