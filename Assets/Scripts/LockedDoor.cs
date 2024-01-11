b1d830bc4d1cd2387f748b6d9bfad21f383d22e5
Mxtern <max.toresjo@elev.ga.lbs.se>
2024-01-11 15:38:04 +0100

    trjjyr

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class LockedDoor : Door
 {
-    bool securityKey = false;
 
-    public override void _Update()
+    bool isUnlocked = false;
+
+    public override void _Interact()
     {
-        base._Update();
-
+        if(isUnlocked == true)
+        {
+            open = true;
+        }
     }
-   
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("securityKey"))
         {
-            _Interact();
+            isUnlocked = true;
         }
         
     }
