22d484fd317ea68145760fffe997cc05082b9630
Mxtern <max.toresjo@elev.ga.lbs.se>
2024-01-11 16:23:04 +0100

    d

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -14,7 +14,10 @@ public class LockedDoor : Door
         }
     }
 
-    
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+    }
 
-    
+
 }
