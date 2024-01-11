bacfa9e53ffb845a7498979e310b9c60a79ad8fc
Mxtern <max.toresjo@elev.ga.lbs.se>
2024-01-11 16:16:39 +0100

    f

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class LockedDoor : Door
 {
-
+    //max gjort detta
     public bool isUnlocked = false;
 
     public override void _Interact()
@@ -14,17 +14,7 @@ public class LockedDoor : Door
         }
     }
 
-    public override void OnTriggerEnter2D(Collider2D other)
-    {
-        base.OnTriggerEnter2D(other);
-        if (other.gameObject.CompareTag("securityKey"))
-        {
-            isUnlocked = true;
-        }
-    }
+    
 
-    public override void _Update()
-    {
-        base._Update();
-    }
+    
 }
---
+++ Assets/Scripts/SDoor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SDoor : LockedDoor
+{ 
+    //Max gjort detta
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+        if (other.gameObject.CompareTag("securityKey"))
+        {
+            isUnlocked = true;
+        }
+    }
+}
