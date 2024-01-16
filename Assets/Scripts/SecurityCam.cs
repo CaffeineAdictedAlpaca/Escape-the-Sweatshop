7f152f34e0ceaa1a413716a8d1fa9940eca8965c
Derrick <122612128+Kaduee@users.noreply.github.com>
2024-01-16 15:46:18 +0100

    SecurityCam

---
+++ Assets/Scripts/SecurityCam.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] 
+    public float rotationSpeed;
+    private bool rotateClockwise = true;
+
+    void Update()
+    {
+        print(transform.rotation.z);
+
+        if (transform.rotation.z > 0.5f)
+        {
+            rotateClockwise = !rotateClockwise;
+        }
+        if (transform.rotation.z < -0.5f)
+        {
+            rotateClockwise = !rotateClockwise;
+        }
+
+        if (rotateClockwise == true)
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+
+        if (rotateClockwise == false)
+        {
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
