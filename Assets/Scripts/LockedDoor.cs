3014033d2069919a02dab4f0aa6365d30073e7d4
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2024-01-17 15:23:45 +0100

    a

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -9,23 +9,23 @@ public class LockedDoor : Door
 
     public override void _Interact()
     {
-        if(isUnlocked == true)
+        if(isUnlocked == true)//Max
         {
             open = true;
         }
-        else
+        else//Sixten
         {
             StartCoroutine(doorLocked());
         }
     }
-    IEnumerator doorLocked()//lets the player know the door is locked
+    IEnumerator doorLocked()//lets the player know the door is locked -Sixten
     {
         doorLockedText.SetActive(true);
         yield return new WaitForSeconds(1.5f);
         doorLockedText.SetActive(false);
     }
 
-    public override void OnTriggerEnter2D(Collider2D other)
+    public override void OnTriggerEnter2D(Collider2D other)//Max
     {
         base.OnTriggerEnter2D(other);
     }
