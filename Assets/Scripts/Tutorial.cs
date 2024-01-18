f450bf6c122876878658de7ece619b92a24e4657
wendasect <santiago.holm.goya@gmail.com>
2024-01-18 13:09:43 +0100

    Update Tutorial.cs

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Tutorial : MonoBehaviour
 {
 
+    public Image w;
+    public Image a;
+    public Image s;
+    public Image d;
+    public Image interact;
+    public Image q;
+    public Image shift;
 
 
 
@@ -18,6 +26,48 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            w.enabled = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            a.enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            s.enabled = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            d.enabled = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            q.enabled = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            interact.enabled = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            shift.enabled = false;
+        }
+
         
+
+
     }
 }
