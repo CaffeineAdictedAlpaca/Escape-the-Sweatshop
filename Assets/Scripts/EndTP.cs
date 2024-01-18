0b4111c5489ec1bafa6bb46ce421d0b6fc3e8de4
Adrian <113990519+AdrianDeveloper@users.noreply.github.com>
2024-01-19 00:11:43 +0100

    endscene improvement

--- Assets/Scripts/EndTP.cs
+++ Assets/Scripts/EndTP.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndTP : MonoBehaviour
 {
+    public Image blackScreen;
     [SerializeField] Transform endTP;
     [SerializeField] Transform player;
+    IEnumerator TP()
+    {
+        for (float i = 0; i <= 1.1f; i += 10f * Time.deltaTime)//fade in the black screen -Sixten
+        {
+            yield return new WaitForSeconds(0.01f);
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, i);
+        }
+
+        player.position = endTP.position;//teleports the player to end -Sixten
+        yield return new WaitForSeconds(2.5f);
+
+        for (float i = 0.9f; i >= 0; i -= 10f * Time.deltaTime)//fade out the black screen -Sixten
+        {
+            yield return new WaitForSeconds(0.01f);
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, i);
+        }
+    }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-         player.position = endTP.position;
+        StartCoroutine(TP());
     }
 }
