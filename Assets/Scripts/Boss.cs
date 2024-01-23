a7515e41df511a0d3795438dcb8ad754709f3a41
CaffeineAdictedAlpaca <119925733+CaffeineAdictedAlpaca@users.noreply.github.com>
2024-01-23 15:43:20 +0100

    level design

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public float bossMaxHealth;
+    private float bossHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bossHealth = bossMaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartBossfight()
+    {
+
+    }
+    private void Bossgight()
+    {
+
+    }
+    private void StopBossfight()
+    {
+
+    }
+}
